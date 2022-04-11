8d72cfc5e382cc9522be2d72a253cbadad9bab7c
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-04-11 14:00:21 -0500

    refactored structure

--- profile.js
+++ profile.js
@@ -4,11 +4,11 @@ const dropdown = document.getElementById('dropdown');
 const createDropDownHTML = (_) => {
 	//language=HTML
 	return `
-        <div class="flex flex-col bg-white text-center text-sm lg:text-xl">
+        <div class="flex flex-col bg-white text-center lg:text-xl">
             <a class="hover:bg-slate-200 mx-2" href="weathermap/index.html" target="_blank">Weather Map</a>
             <a class="hover:bg-slate-200 mx-2" href="bankist/index.html" target="_blank">Bankist Application</a>
             <a class="hover:bg-slate-200 mx-2" href="coffee-project/main.html" target="_blank">Coffee Application</a>
-            <a class="hover:bg-slate-200 mx-2" href="movie-project/index.html" target="_blank">Movie Database</a>
+            <a class="hover:bg-slate-200" href="movie-project/index.html" target="_blank">Movie Database</a>
         </div>`;
 };
 
